f7e6f0803c3d8bdf4c094b7472a3e96e5af80aab
nfomenko1@mail.ru <nfomenko1@mail.ru>
2026-09-25 13:05:07 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Served from https://<user>.github.io/wedding-invitation/ on GitHub Pages
+  base: '/wedding-invitation/',
   plugins: [react()],
 })
